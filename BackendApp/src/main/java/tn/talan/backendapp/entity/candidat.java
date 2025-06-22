e571cce535c2fd88813ab70337ec715b1355ab0f
abir-ommezzine <abir.ommezzine@ensi-uma.tn>
2025-06-22 13:19:24 +0100

    updated candidat crud

--- BackendApp/src/main/java/tn/talan/backendapp/entity/candidat.java
+++ BackendApp/src/main/java/tn/talan/backendapp/entity/candidat.java
@@ -1,26 +1,43 @@
 package tn.talan.backendapp.entity;
 import jakarta.persistence.*;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
 import tn.talan.backendapp.enums.statut;
 
-import lombok.Data;
 
 @Entity
 @Table(name="candidat")
-@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class candidat {
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+    @Column(name = "nom")
     private String nom;
+    @Column(name = "prenom")
     private String prenom;
+    @Column(name = "email", unique = true)
     private String email;
+    @Column(name = "telephone")
     private String telephone;
+    @Column(name = "competences")
     private String competences;
+    @Column(name = "poste")
     private String poste;
+    @Column(name = "cv_path")
     private String cvPath;
+    @Column(name = "profile_photo_path")
     private String profilePhotoPath;
 
     @Enumerated(EnumType.STRING)
+    @Column(name = "statut")
     private statut statut;
 
 
